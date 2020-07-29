0064886e3281dfbfaab9c8035d6a32836ebc2c75
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-07-29 15:33:07 -0700

    add create view and get/post route to handle
    Co-authored-by: Jeff Kim <kim2jy@hotmail.com>

--- Controllers/CusinesController.cs
+++ Controllers/CusinesController.cs
@@ -21,5 +21,17 @@ namespace BestRestaurants.Controllers
       return View(model);
     }
     
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost] 
+    public ActionResult Create(Cuisine cuisine)
+    {
+      _db.Cuisines.Add(cuisine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
